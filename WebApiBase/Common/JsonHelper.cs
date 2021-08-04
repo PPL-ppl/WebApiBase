535d6167021846e2148c13602b14313110fdc48d
Wzl <https://gitee.com/article_liang/csharp.git>
2021-08-04 13:57:29 +0800

    增加Json处理数据

--- WebApiBase/Common/JsonHelper.cs
+++ WebApiBase/Common/JsonHelper.cs
@@ -1,4 +1,7 @@
-﻿namespace WebApiBase.Common
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiBase.Common
 {
     public class JsonHelper
     {
@@ -11,6 +14,23 @@
         public static string toJson(object result)
         {
             return Newtonsoft.Json.JsonConvert.SerializeObject(result);
+        }
+
+        //取Json数据中的字段数据
+        public static string toStr(string end, string start)
+        {
+            Object obj = Newtonsoft.Json.JsonConvert.DeserializeObject(end);
+            Newtonsoft.Json.Linq.JObject js = obj as Newtonsoft.Json.Linq.JObject;
+            var result = js[end].ToString();
+            return result;
+        }
+
+        //将Json数据转换为T数组
+        public static T[] toStrs<T>(string result)
+        {
+            List<T> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<T>>(result);
+            T[] sArray = list.ToArray();
+            return sArray;
         }
     }
 }
