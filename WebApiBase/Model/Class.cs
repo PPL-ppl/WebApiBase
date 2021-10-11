0e2ccd605833d32f72e40d60115fa0a2b5ab1e6d
Wzl <1942400019@qq.com>
2021-10-11 20:38:18 +0800

    Wzl的提交

---
+++ WebApiBase/Model/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBase.Model
+{
+    public class Class
+    {
+        int Jetbrains;
+        int License;
+    }
+}
